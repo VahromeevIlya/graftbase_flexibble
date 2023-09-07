1acb1d4b5f4df854c11658dc24f3c89d5422aa5b
vahromeevilya-widestudio <98152955+vahromeevilya-widestudio@users.noreply.github.com>
2023-09-07 12:27:23 +0300

    auth

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,18 +1,24 @@
-import { g, auth, config } from "@grafbase/sdk";
+import { g, config, auth } from "@grafbase/sdk";
 
-const User = g.model("User", {
-	name: g.string().length({ min: 2, max: 20 }),
-	email: g.string().unique(),
-	avatarUrl: g.url(),
-	description: g.string(),
-	githubUrl: g.url().optional(),
-	linkedInUrl: g.url().optional(),
-	projects: g
-		.relation(() => Project)
-		.list()
-		.optional(),
-});
+// @ts-ignore
+const User = g
+	.model("User", {
+		name: g.string().length({ min: 2, max: 100 }),
+		email: g.string().unique(),
+		avatarUrl: g.url(),
+		description: g.string().length({ min: 2, max: 1000 }).optional(),
+		githubUrl: g.url().optional(),
+		linkedinUrl: g.url().optional(),
+		projects: g
+			.relation(() => Project)
+			.list()
+			.optional(),
+	})
+	.auth((rules) => {
+		rules.public().read();
+	});
 
+// @ts-ignore
 const Project = g
 	.model("Project", {
 		title: g.string().length({ min: 3 }),
@@ -28,6 +34,15 @@ const Project = g
 		rules.private().create().delete().update();
 	});
 
+const jwt = auth.JWT({
+	issuer: "grafbase",
+	secret: g.env("NEXTAUTH_SECRET"),
+});
+
 export default config({
 	schema: g,
+	auth: {
+		providers: [jwt],
+		rules: (rules) => rules.private(),
+	},
 });
---
+++ lib/actions.ts
@@ -0,0 +1,48 @@
+import { GraphQLClient } from "graphql-request";
+
+import { createUserMutation, getUserQuery } from "@/graphql";
+import { ProjectForm } from "@/common.types";
+
+const isProduction = process.env.NODE_ENV === "production";
+const apiUrl = isProduction
+	? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || ""
+	: "http://127.0.0.1:4000/graphql";
+const apiKey = isProduction
+	? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || ""
+	: "letmein";
+const serverUrl = isProduction
+	? process.env.NEXT_PUBLIC_SERVER_URL
+	: "http://localhost:3000";
+
+const client = new GraphQLClient(apiUrl);
+
+const makeGraphQLRequest = async (query: string, variables = {}) => {
+	try {
+		return await client.request(query, variables);
+	} catch (err) {
+		throw err;
+	}
+};
+export const createUser = (name: string, email: string, avatarUrl: string) => {
+	client.setHeader("x-api-key", apiKey);
+
+	const variables = {
+		input: {
+			name: name,
+			email: email,
+			avatarUrl: avatarUrl,
+		},
+	};
+
+	return makeGraphQLRequest(createUserMutation, variables);
+};
+
+//export const getUserProjects = (id: string, last?: number) => {
+//  client.setHeader("x-api-key", apiKey);
+//  return makeGraphQLRequest(getProjectsOfUserQuery, { id, last });
+//};
+
+export const getUser = (email: string) => {
+	client.setHeader("x-api-key", apiKey);
+	return makeGraphQLRequest(getUserQuery, { email });
+};
