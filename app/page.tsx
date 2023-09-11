1d6ad07f83800261e2e6c1f33fbdb66cde1c8eb8
vahromeevilya-widestudio <98152955+vahromeevilya-widestudio@users.noreply.github.com>
2023-09-11 11:15:36 +0300

    loadmore

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import { ProjectInterface } from "@/common.types";
 import Categories from "@/components/Categories";
+import LoadMore from "@/components/LoadMore";
 import ProjectCard from "@/components/ProjectCard";
 import { fetchAllProjects } from "@/lib/actions";
 
@@ -16,12 +17,17 @@ type ProjectSearch = {
 };
 type SearchParams = {
 	category?: string | null;
+	endcursor?: string | null;
 };
 type Props = {
 	searchParams: SearchParams;
 };
-const Home = async ({ searchParams: { category } }: Props) => {
-	const data = (await fetchAllProjects(category)) as ProjectSearch;
+export const dynamic = "force-dynamic";
+export const dynamicParams = true;
+export const revalidate = 0;
+
+const Home = async ({ searchParams: { category, endcursor } }: Props) => {
+	const data = (await fetchAllProjects(category, endcursor)) as ProjectSearch;
 
 	const projectsToDisplay = data?.projectSearch?.edges || [];
 
@@ -36,6 +42,8 @@ const Home = async ({ searchParams: { category } }: Props) => {
 			</section>
 		);
 	}
+
+	const pagination = data?.projectSearch?.pageInfo;
 	return (
 		<section className="flex-start flex-col paddings mb-16">
 			<Categories />
@@ -53,7 +61,12 @@ const Home = async ({ searchParams: { category } }: Props) => {
 				))}
 				C
 			</section>
-			<h1>LoadMore</h1>
+			<LoadMore
+				startCursor={pagination.startCursor}
+				endCursor={pagination.endCursor}
+				hasPreviousPage={pagination.hasPreviousPage}
+				hasNextPage={pagination.hasNextPage}
+			/>
 		</section>
 	);
 };
